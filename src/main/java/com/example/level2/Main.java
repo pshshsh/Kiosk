51470339b32243ffcc12ba9a241f55f94d43ef25
park seong ho <hgg0305@daum.net>
2025-01-13 16:41:29 +0900

    레벨2 수정

--- src/main/java/com/example/level2/Main.java
+++ src/main/java/com/example/level2/Main.java
@@ -36,11 +36,11 @@ public class Main {
         System.out.println("프로그램을 종료합니다");
         System.exit(0);//프로그램 종료
       }
-      if (num >= 1 && num <= menuItems.size()) { // 입력이 1이상 최대 번호 이하 각번호의 맞는 정보표시
+      else if (num >= 1 && num <= menuItems.size()) { // 입력이 1이상 최대 번호 이하 각번호의 맞는 정보표시
         System.out.printf("메뉴 이름: %s, 가격: %.1f W, 설명: %s%n",
-            menuItems.get(num).getName(),
-            menuItems.get(num).getCost() / 1000.0,
-            menuItems.get(num).getEx());
+            menuItems.get(num-1).getName(),
+            menuItems.get(num-1).getCost() / 1000.0,
+            menuItems.get(num-1).getEx());
       }
       }
       }
